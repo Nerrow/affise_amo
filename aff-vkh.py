174adf3eddde118daedc19eca5f193289cd3b5d9
Nerrow <memeboys1337@gmail.com>
2020-11-04 20:14:04 +0700

    update

--- aff-vkh.py
+++ aff-vkh.py
@@ -20,9 +20,16 @@ def affise():
         lead_id = request.form.get('leads[status][0][id]')
         get_amo_update_lead = rq.get(f'https://podruzhki.amocrm.ru/api/v4/leads/{lead_id}',
                                      headers={'Authorization': f'Bearer {amo_token}'})
-        clickid = get_amo_update_lead.json()['custom_fields_values'][1]['values'][0]['value']
-        conversation = rq.get(f"https://offers-podruge.affise.com/postback?clickid={clickid}")
+        if get_amo_update_lead.json()['custom_fields_values'][1]['field_name'] == 'CLICKID':
+            clickid = get_amo_update_lead.json()['custom_fields_values'][1]['values'][0]['value']
+            conversation = rq.get(f"https://offers-podruge.affise.com/postback?clickid={clickid}")
+        else:
+            pass
     except KeyError as e:
         print(f"ERROR: {e}")
 
     return 'ok'
+
+
+if __name__ == '__main__':
+    application.run()
